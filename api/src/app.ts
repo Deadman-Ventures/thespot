b55ee2b80d5a756138b866a1a65a9d89a0473681
Kevin Castell <castellkevin10@gmail.com>
2024-01-30 19:47:59 -0700

    setup supertokesn core and connect to the db little change to liquibase

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,18 +3,86 @@ import cors from 'cors';
 import { routes } from "./routes/index.js"
 import { itineraryRoutes } from './routes/itineraryRoutes.js';
 import { activityRoutes } from './routes/activityRoutes.js'
+import supertokens from "supertokens-node";
+import Session from "supertokens-node/recipe/session";
+import ThirdPartyEmailPassword from "supertokens-node/recipe/thirdpartyemailpassword";
+import { middleware } from "supertokens-node/framework/express";
+import { errorHandler } from "supertokens-node/framework/express";
+
 (async function () {
   await import("dotenv/config")
 })()
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(cors());
+supertokens.init({
+  framework: "express",
+  supertokens: {
+    // https://try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
+    connectionURI: "https://try.supertokens.com",
+    // apiKey: <API_KEY(if configured)>,
+  },
+  appInfo: {
+    // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
+    appName: process.env.APP_NAME,
+    apiDomain: process.env.API_DOMAIN,
+    websiteDomain: process.env.WEBSITE_DOMAIN,
+    apiBasePath: "/auth",
+    websiteBasePath: "/auth"
+  },
+  recipeList: [
+    ThirdPartyEmailPassword.init({
+      // We have provided you with development keys which you can use for testing.
+      // IMPORTANT: Please replace them with your own OAuth keys for production use.
+      providers: [{
+        config: {
+          thirdPartyId: "google",
+          clients: [{
+            clientId: "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
+            clientSecret: "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"
+          }]
+        }
+      }, {
+        config: {
+          thirdPartyId: "github",
+          clients: [{
+            clientId: "467101b197249757c71f",
+            clientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd"
+          }]
+        }
+      }, {
+        config: {
+          thirdPartyId: "apple",
+          clients: [{
+            clientId: "4398792-io.supertokens.example.service",
+            additionalConfig: {
+              keyId: "7M48Y4RYDL",
+              privateKey:
+                "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
+              teamId: "YWQCXGJRJL",
+            }
+          }]
+        }
+      }],
+    }),
+    Session.init() // initializes session features
+  ]
+});
+
+app.use(cors({
+  origin: process.env.WEBSITE_DOMAIN,
+  allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
+  credentials: true,
+}));
+
+// IMPORTANT: CORS should be before the below line.
+app.use(middleware());
 app.use('/', routes)
 app.use('/itinerary', itineraryRoutes)
 app.use('/activity', activityRoutes)
 
+app.use(errorHandler())
 app.use((err, req, res, next) => {
   res.status(err.status || 400).json({
     success: false,
--- client/src/Types/Config.ts
+++ client/src/Types/Config.ts
@@ -1,1 +1,3 @@
 export const API_ROOT = process.env.API_ROOT
+export const APP_NAME = process.env.APP_NAME
+export const WEBSITE_DOMAIN = process.env.WEBSITE_DOMAIN
